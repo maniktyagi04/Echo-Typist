e5ed9e30c3582fc7b36ff212f335005a902de050
Lomesh Sonkeshriya <luckysonkeshriya@Lomeshs-MacBook-Pro.local>
2025-03-29 15:36:12 +0530

    first commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const feedback = document.getElementById('feedback');
 const scoreDisplay = document.getElementById('score');
 const timerDisplay = document.getElementById('timer');
 
-const words = ['hello', 'world', 'javascript', 'developer', 'game', 'speed', 'keyboard', 'audio', 'challenge', 'typing', 'echo', 'krushna', 'function', 'variable', 'future'];
+const words = ['hello', 'world', 'javascript', 'developer', 'game', 'speed', 'keyboard', 'audio', 'challenge', 'typing', 'echo', 'krushna', 'function', 'variable', 'future', 'Lomesh'];
 let currentWord = '';
 let score = 0;
 let timeLeft = 30;
